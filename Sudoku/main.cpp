e0d22b50c4ce4fe562dce5527a89188879c95479
rs3254 <raysabb3254@gmail.com>
2018-06-13 20:12:48 -0400

    Sudoku Puzzle opened in new file

--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -9,6 +9,8 @@
 #include <iostream>
 #include <stdio.h>
 #include "DrawBoard.hpp"
+#include <fstream>
+#include <unistd.h>
 
 using namespace std;
 
@@ -16,40 +18,67 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
 
+    system("rm -f Solutions.txt");
+    system("rm -f Soduku.txt");
+    
     srand(int(time(0)));
-
     int finished;
-        
     
-    cout<<"Below is the sudoku puzzle. Solve it on another piece of paper. Press 1 when finished to display solutions.\n"<<endl;
+    fstream sudokuFile;
+    fstream solutionFile;
+    sudokuFile.open("Sudoku.txt",fstream::out);
+    solutionFile.open("Solutions.txt",fstream::out);
     
+    
+    cout<<"Generating Puzzle.\n"<<endl;
     Board *boardObj = new Board;
+    streambuf *backup;
+    backup = cout.rdbuf();
+    
+    cout.rdbuf(sudokuFile.rdbuf());
+    cout<<"Press 1 in Xcode console when finished to display solutions\n"<<endl;
+    cout<<"\n"<<endl;
     boardObj->drawBoard();
+    sudokuFile.close();
+
+    
+    
+    // write into solutions file
+    cout.rdbuf(solutionFile.rdbuf());
+    cout<<"Solutions\n"<<endl;
+    boardObj->drawBoard(1);
+    cout<<"\n"<<endl;
+    solutionFile.close();
+
+    
+    cout.rdbuf(backup);
+    system("Open Sudoku.txt");
+    
+    cout<<"The Sudoku puzzle is in Sudoku.txt. Solve it. Press 1 when finished to display solutions.\n"<<endl;
+    
+
+    
     cout<<""<<endl;
     
     cin>>finished;
     if(finished == 1){
-        cout<<" "<<endl;
-        cout<<" "<<endl;
-        cout<<"solution below"<<endl;
-        boardObj->drawBoard(1);
 
+        system("Open Solutions.txt");
 
     }
     else{
+        
         cout<<"invalid input program terminating"<<endl;
     }
+
+    
+
+
 
     delete boardObj;
     
     
     
-    
-//    
-//    vector<int> arr = shuffle();
-//    for(int i =0; i<9; i++){
-//        cout<<arr[i]<<endl;
-//    }
-//
+
     return 0;
 }
